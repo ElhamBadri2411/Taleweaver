db4ea41afa39be5d7eea142d61686982cedfd24b
elhambadri2411 <elhambadri24@gmail.com>
2024-06-18 15:56:47 -0400

    task: created small form for image generation on frontend

--- taleweaver-frontend/src/app/app.routes.ts
+++ taleweaver-frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ImageGeneratorComponent } from './components/image-generator/image-generator.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'image-generator', component: ImageGeneratorComponent },
+  { path: '', redirectTo: 'image-generator', pathMatch: 'full' }
+];
