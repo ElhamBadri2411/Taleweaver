31b8102c68ae3116eead0eca3c26cc021ae45638
Katy Chen <katy.chen@mail.utoronto.ca>
2024-06-23 19:28:52 -0400

    feat: frontend of dashboard (no link to backend yet)

--- taleweaver-frontend/src/app/app.routes.ts
+++ taleweaver-frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { ImageGeneratorComponent } from './components/image-generator/image-generator.component';
 import { NewStoryFormComponent } from './components/new-story-form/new-story-form.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
   { path: 'image-generator', component: ImageGeneratorComponent },
   { path: 'new-story', component: NewStoryFormComponent },
   { path: '', redirectTo: '', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent }
 ];
