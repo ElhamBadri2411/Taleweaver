ef074cdcb6634cedca1ea473bb3430fc4b611e02
elhambadri2411 <elhambadri24@gmail.com>
2024-07-03 23:00:26 -0400

    task: added text auto saving w/ debounce and improved ui

--- taleweaver-frontend/src/app/components/image-generator/image-generator.component.ts
+++ taleweaver-frontend/src/app/components/image-generator/image-generator.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit, Input, OnChanges, SimpleChanges, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges, Output, EventEmitter, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
 import { ImagesService } from '../../services/images.service';
 import { environment } from '../../../environments/environment';
-import { heroSparklesSolid } from '@ng-icons/heroicons/solid';
+import { heroSparklesSolid, heroXCircleSolid } from '@ng-icons/heroicons/solid';
 import { PageService } from '../../services/page.service';
+import { debounceTime, takeUntil, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-image-generator',
@@ -19,12 +20,15 @@ import { PageService } from '../../services/page.service';
   ],
   templateUrl: './image-generator.component.html',
   styleUrl: './image-generator.component.css',
-  viewProviders: [provideIcons({ heroSparklesSolid })],
+  viewProviders: [provideIcons({ heroSparklesSolid, heroXCircleSolid })],
 })
-export class ImageGeneratorComponent implements OnInit, OnChanges {
+export class ImageGeneratorComponent implements OnInit, OnChanges, OnDestroy {
   @Input() bookId: string;
   @Input() pageId: number | null;
   @Output() imageGenerated = new EventEmitter<void>()
+
+  private destroy = new Subject<void>();
+  private autoSave = new Subject<string>();
 
 
   title: string = 'Test title'
@@ -34,6 +38,10 @@ export class ImageGeneratorComponent implements OnInit, OnChanges {
     environment.apiUrl +
     'generated-images/20cbbc5619f1c04a5b32f0025461acf8.png';
   isGeneratingImage: boolean = false;
+  isSaving: boolean = false;
+  isEditing: boolean = false;
+
+
 
   constructor(
     private fb: FormBuilder,
@@ -47,12 +55,55 @@ export class ImageGeneratorComponent implements OnInit, OnChanges {
 
   ngOnInit(): void {
     this.loadPageData()
+    this.autoSaveSetup()
   }
 
   ngOnChanges(changes: SimpleChanges): void {
     this.loadPageData()
   }
 
+
+  ngOnDestroy(): void {
+    this.destroy.next()
+    this.destroy.complete()
+  }
+
+
+
+  autoSaveSetup(): void {
+    this.form.get('text')?.valueChanges.pipe(
+      takeUntil(this.destroy)
+    ).subscribe((val) => {
+      this.isEditing = true;
+      this.autoSave.next(val)
+    })
+
+    this.autoSave.pipe(
+      debounceTime(2000),
+      takeUntil(this.destroy)
+    ).subscribe(text => {
+      this.isEditing = false;
+      this.savePage(text)
+    })
+  }
+
+  savePage(text: string): void {
+    if (this.pageId !== null) {
+      this.isSaving = true;
+      this.pagesService.updatePage(this.pageId!, text).subscribe({
+        next: (res) => {
+          console.log("SAVED")
+          this.isSaving = false
+        },
+        error: (error) => {
+          console.error(error)
+          this.isSaving = false
+        }
+      })
+
+    }
+
+  }
 
   loadPageData(): void {
     this.imageUrl = ''
