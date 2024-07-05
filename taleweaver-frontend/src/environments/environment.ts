0fd38300d6c10711e3df02a92445ae5ee54accf9
elhambadri2411 <elhambadri24@gmail.com>
2024-07-04 23:44:00 -0400

    task: added y-protocol dependency for awareness in the future

--- taleweaver-frontend/src/environments/environment.ts
+++ taleweaver-frontend/src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:3000/api/',
+  wsUrl: 'http://localhost:1234',
   clientId: '',
   redirectUri: window.location.origin,
   postLogoutRedirectUri: `${window.location.origin}/signin`,
