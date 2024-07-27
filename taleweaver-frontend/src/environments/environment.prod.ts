dbbfb16fba70e5e94473a4e557266b76574b4e1b
elhambadri2411 <elhambadri24@gmail.com>
2024-07-27 13:00:16 -0400

    task: created docker ocmpose and github actions

--- taleweaver-frontend/src/environments/environment.prod.ts
+++ taleweaver-frontend/src/environments/environment.prod.ts
@@ -1,1 +1,9 @@
-export const environment = {};
+export const environment = {
+  production: true,
+  apiUrl: 'http://35.182.94.15:3000/api/',
+  wsUrl: 'ws://35.182.94.15:3000',
+  clientId: '232201998992-q14m6e4ptncjclv72qi0rg23semeiv8h.apps.googleusercontent.com',
+  redirectUri: window.location.origin,
+  postLogoutRedirectUri: `${window.location.origin}/signin`,
+  originLocation: window.location.origin,
+};
